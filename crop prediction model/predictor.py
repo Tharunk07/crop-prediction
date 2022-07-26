0f66f0f9110119a82dfc45f114ee8c9c5001ec67
Tharun K <97456744+Tharunk07@users.noreply.github.com>
2022-07-26 19:03:48 +0530

    Delete crop prediction model directory

--- crop prediction model/predictor.py
+++
@@ -1,5 +0,0 @@
-import pickle
-model=pickle.load(open("model.pkl","rb"))
-def crop(nitrogen,phosphorus,potassium,temperature,humidity,rainfall):
-    ans=model.predict([[nitrogen,phosphorus,potassium,temperature,humidity,rainfall]])
-    return ans
